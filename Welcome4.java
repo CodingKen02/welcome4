3c32e16b92a5e9f139b80a0c61d1836142455a9b
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-01-25 14:16:33 -0600

    Initial commit

---
+++ Welcome4.java
@@ -0,0 +1,28 @@
+/*
+Name: Kennedy Keyes
+Date: February 11, 2021
+Name of Program: Welcome4.java
+Description: This program will display multiple lines with method 
+System.out.printf. 
+ */
+package welcome4;
+
+/**
+ *
+ * @author codingken
+ */
+public class Welcome4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // main method begins execution of Java application
+    public static void main(String[] args) 
+    {
+        // display multiple lines with method System.out.printf
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+        
+    } // end method main
+    
+} // end class Welcome4
